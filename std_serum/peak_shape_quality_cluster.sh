d3b69dc708711442053936013527b61ffa10b6f4
Mar Garcia-Aloy <mar.garcia.aloy@gmail.com>
2020-02-13 10:28:47 +0100

    Update peak_shape_quality_cluster.sh
    correct file content

--- std_serum/peak_shape_quality_cluster.sh
+++ std_serum/peak_shape_quality_cluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-R-3.6.1-BioC3.10 --file="/home/mgarciaaloy/git/EuracBiomedicalResearch/lcms-standards/std_serum/peak_shape_quality_cluster.R"
+/shared/bioinf/R/R-3.6.1-BioC3.10/bin/R --file="/home/mgarciaaloy/git/EuracBiomedicalResearch/lcms-standards/std_serum/peak_shape_quality_cluster.R"
 
